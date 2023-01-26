8210514b877682da119c45bed44fd11752e3183f
Jiyun <jiyumi00@naver.com>
2023-01-27 01:21:03 +0900

    #1001

---
+++ DAY01/1001.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sum(int first_num, int second_num) {
+	return first_num - second_num;
+}
+int main()
+{
+	int first_num, second_num;
+	int result;
+	cin >> first_num >> second_num;
+	
+	result = sum(first_num, second_num);
+	
+	cout << result;
+}
